83d83ad7b1d6064fa0c426d8d23f18fec4bbcb86
shrke <shreyamkela@gmail.com>
2019-02-02 01:58:16 -0800

    global object and window

--- 02_execution_context/public/003_globals.js
+++ 02_execution_context/public/003_globals.js
@@ -1,2 +1,9 @@
-// Even with an empty .js file, a global object is created which can be refered to by 'this'
-console.log('hey');
+// Even with an empty .js file, when the file is run, an executon context (which is a wrapper code or javascript engine that runs and wraps around all of your code) is created  and a global object which refers to that particular 'window' object. At the global level, 'this' also referes to the global object
+// type 'this' on console and hit enter
+// type 'window' on console and hit enter
+
+// All the variables and functions and everything declared inside the global object gets attached to the global object
+var a = 10;
+// type 'a' on console and hit enter
+// type 'window.a' on console and hit enter. The result is the same as 'a'. 'a' was attached to 'window'/global obj
+
--- 02_execution_context/server.js
+++ 02_execution_context/server.js
@@ -3,13 +3,13 @@ const express = require('express');
 var app = express(); 
 
 app.use(express.static(__dirname + '/public')); // Public folder call. This is express middleware. It deals public static files. Put this after maintanance express middleware
-// express.static lets include external JavaScript file into an HTML file (main.html) when serving that HTML file with a response object in expressjs
+// express.static lets include external JavaScript file into an HTML file (index.html) when serving that HTML file with a response object in expressjs
 // Include all such static content into the public folder
 // Note that nowadays, html, css, js, etc files are not sent by the backend server to the frontend, everytime a request is made. Nowadays with frameworks like react and angular, html and js and all the file content is sent only on the first load of the website. This is possible with the help of a backend in nodejs which will send the files and a frontend 'server' on react/angular that receive the files. Next time the website is loaded, only json data is sent. And only relevant stuff change on the page and the rest remains the same. That is it is all dynamic. In older times everything was static and therefore sending the whole file is called as serving static files. express.static is thus used to serve static content. Nowadays it's mostly about the dynamic content
 // Refer - https://stackoverflow.com/questions/29087318/how-do-i-include-an-external-javascript-file-when-serving-an-html-file-with-a-re#
 
 app.get('/', (req, res) => { 
-    res.sendFile('main.html'); //sendFile sends a whole file to be rendered. __dirname is used when full path is required, instead of './' Here we are not using res.render as res.render can render a view and not a file. So to render an html file with a view we have to create views and view engine 
+    res.sendFile(__dirname + '/index.html'); //sendFile sends a whole file to be rendered. __dirname is used when full path is required, instead of './' Here we are not using res.render as res.render can render a view and not a file. So to render an html file with a view we have to create views and view engine 
 });
 
 app.listen(3000, () => { 
