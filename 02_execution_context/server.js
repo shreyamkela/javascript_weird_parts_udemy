31682b8a7db46dd22f4049a6ad94e917c61bb191
shrke <shreyamkela@gmail.com>
2019-02-01 23:27:39 -0800

    globals in javascript

--- 02_execution_context/server.js
+++ 02_execution_context/server.js
@@ -1,16 +1,11 @@
 const express = require('express');
 
-var app = express(); // make an app/application server that handles requests
+var app = express(); 
 
-app.get('/', (req, res) => { // / is the root or landing page/route of your application/website. Here we are defining what happens when user visits the '/' page. req defines all the data/flags etc the user has sent i.e req can be set by the user input and then by analysis req we can determine what to send back. res is the response we send back
-    //res.send('Hello Express!');
-    //res.send('<h1>Hello Express!</h1>'); // Content type that gets sent is text/html
-    path = '02_execution_context'
-    res.render('02_execution_context/003_global.html');
+app.get('/', (req, res) => { 
+    res.sendFile(__dirname + '/003_global.html'); //sendFile sends a whole file to be rendered. __dirname is used when full path is required, instead of './' Here we are not using res.render as res.render can render a view and not a file. So to render an html file with a view we have to create views and view engine 
 });
 
-// To let the application listen/start at a particular port number:
-// app.listen(3000);
-app.listen(3000, () => { // We can also pass a function as the second argument to listen
+app.listen(3000, () => { 
     console.log('Server is up on port 3000');
 });
